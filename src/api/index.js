ea35e5ece7c0c66b1aaa38148a6547dad128932a
AlexTsao741118 <andy741118@gmail.com>
2024-06-24 23:31:20 +0800

    Update current development progress

---
+++ src/api/index.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+
+// ==============進行基本的配置==============
+// 後端接口的域名設置
+axios.defaults.baseURL = 'http://127.0.0.1:8000'
+// 請求成功還是失敗的狀態碼範圍
+axios.defaults.validateStatus = function (status) {
+    return true
+}
+
+axios.defaults.withCredentials = true
+
+
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    return config;
+  }, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  });
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+    // 2xx 范围内的状态码都会触发该函数。
+    // 对响应数据做点什么
+    return response;
+  }, function (error) {
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么
+    return Promise.reject(error);
+  });
+
+
+// 後端請求的接口
+export default {
+
+}
--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@ import App from './App.vue'
 import router from './router'
 // element plus相關庫的導入
 import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 // 導入暗黑模式主題
 import 'element-plus/theme-chalk/dark/css-vars.css'
+// 導入項目的全局css樣式文件
+import 'style/global.css'
 
 // 創建一個應用
-app = createApp(App)
+const app = createApp(App)
 
 // @ 註冊element plus
 app.use(ElementPlus, {
